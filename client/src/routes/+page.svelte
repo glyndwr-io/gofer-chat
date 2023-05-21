a8948552cee87e5414bfb85a941341b8caaba144
Glyndwr Morris <morrisg22@mymacewan.com>
2023-05-20 21:06:46 -0600

    Implement Chat and Login UI

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,31 +1,162 @@
 <script lang="ts">
 	import { browser } from "$app/environment";
 	import { onMount } from "svelte";
+    import { AppShell } from '@skeletonlabs/skeleton';
+    import { AppRail, AppRailTile, AppBar, Avatar } from '@skeletonlabs/skeleton';
+	import { get, writable, type Writable } from "svelte/store";
+    import { IconMenu2, IconMessageCircle2Filled, IconBrandGolang } from '@tabler/icons-svelte';
+	import { each } from "svelte/internal";
 
-    let input: HTMLInputElement;
-    let output: HTMLPreElement;
+    type MessagEventInbound = {
+        Sender: string,
+        Event: string,
+        Content: string,
+        Channel: string
+    }
+
+    type ChannelMessage = {
+        Sender: string,
+        Content: string,
+    }
+
+    let input: HTMLTextAreaElement;
     let socket: WebSocket;
+    let online = false;
 
     onMount(() => {
         if(!browser) return
 
         socket = new WebSocket("ws://localhost:8080/ws")
 
-        socket.onopen = function () {
-            output.innerHTML += "Status: Connected\n";
+        socket.onopen = () => {
+            online = true
         };
 
-        socket.onmessage = function (e) {
-            output.innerHTML += "Server: " + e.data + "\n";
+        socket.onclose = () => {
+            online = false
+        }
+
+        socket.onmessage = e => {
+            const { Sender, Content, Channel } = JSON.parse(e.data) as MessagEventInbound
+            const temp = channels.get(Channel)
+            if(!temp) return
+            temp.push({ Sender, Content })
+            channels.set(Channel, temp)
+            channels = channels
         };
     })
 
-    function send() {
-        socket.send(input.value);
+    function send(e: KeyboardEvent) {
+        const { key, shiftKey } = e
+
+        if(key !== 'Enter' || shiftKey)
+            return
+
+        socket.send(JSON.stringify({
+            event: 'message',
+            channel: get(storeValue),
+            content: input.value
+        }));
+
         input.value = "";
     }
+
+    let channels = new Map<string, ChannelMessage[]>()
+
+    channels.set('main', [])
+    channels.set('off-topic', [])
+    channels.set('new-members', [])
+
+    const storeValue: Writable<string> = writable('main');
 </script>
 
-<input type="text" bind:this={input}/>
-<button on:click={send}>Send</button>
-<pre bind:this={output}></pre>
+<AppShell>
+	<svelte:fragment slot="header">
+        <AppBar>
+            <svelte:fragment slot="lead">
+                <IconBrandGolang/>
+            </svelte:fragment>
+            <h1>GoferChat</h1>
+            <svelte:fragment slot="trail">
+                {#if online}
+                    <Avatar
+                        border="border-4 border-primary-500"
+                        cursor="cursor-pointer"
+                    />
+                {:else}
+                    <Avatar
+                        border="border-4 border-surface-300-600-token"
+                        cursor="cursor-pointer"
+                    />
+                {/if}
+            </svelte:fragment>
+        </AppBar>
+    </svelte:fragment>
+
+	<svelte:fragment slot="sidebarLeft">
+        <AppRail selected={storeValue}>
+            {#each [...channels.keys()] as channel}
+                <AppRailTile label="#{channel}" value={channel}>
+                    <IconMessageCircle2Filled/>
+                </AppRailTile>
+            {/each}
+        </AppRail>
+    </svelte:fragment>
+
+	<!-- (sidebarRight) -->
+	<!-- (pageHeader) -->
+	<!-- Router Slot -->
+	<div class="chat">
+        
+
+        <div class="message self">
+            <div class="">
+                <Avatar initials="AB" background="bg-primary-500" />
+            </div>
+            
+            <aside class="alert variant-filled">
+                <div class="alert-message">
+                    <h3 class="h3">DisplayName123</h3>
+                    <p>Hello there</p>
+                </div>
+            </aside>
+        </div>
+
+        {#each channels?.get($storeValue) || [] as message}
+            <div class="message">
+                <div class="">
+                    <Avatar initials="JD" background="bg-primary-500" />
+                </div>
+                
+                <aside class="alert variant-ghost">
+                    <div class="alert-message">
+                        <h3 class="h3">{message.Sender}</h3>
+                        <p>{message.Content}</p>
+                    </div>
+                </aside>
+            </div>
+        {/each}
+
+    </div>
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="footer">
+        <textarea bind:this={input} on:keyup={send} name="" id="" cols="30" rows="10"></textarea>
+    </svelte:fragment>
+	<!-- (footer) -->
+</AppShell>
+
+<style>
+    textarea {
+        width: 100%;
+        resize: none;
+    }
+
+    .message {
+        display: flex;
+        gap: 2rem;
+        padding: 2rem;
+    }
+    .message.self {
+        flex-direction: row-reverse;
+    }
+</style>
--- client/src/routes/login/+page.svelte
+++ client/src/routes/login/+page.svelte
@@ -1,3 +1,39 @@
-<form action="">
-    <input type="text" name="display_name">
+<script lang="ts">
+	import { goto } from "$app/navigation";
+
+    async function register(e: SubmitEvent) {
+        const f = new FormData(e.target as HTMLFormElement)
+        await fetch('/login.json', {
+            method: 'POST',
+            body: f
+        })
+        goto('/')
+    }
+</script>
+
+<form class="login-wrapper" on:submit|preventDefault={register}>
+    <div class="card p-4">
+        <header class="card-header">
+            <h1 class="h1 text-center">GoferChat</h1>
+        </header>
+        <section class="p-4">
+            <label class="label">
+                <span>Display Name</span>
+                <input class="input p4" type="text" placeholder="SamS3pi0l" name="display-name" required/>
+            </label>
+        </section>
+        <footer class="card-footer flex jusitfy-center">
+            <button class="btn variant-filled-primary">Join</button>
+        </footer>
+    </div>
+    
 </form>
+
+<style>
+    .login-wrapper {
+        width: 100%;
+        height: 100%;
+        display: grid;
+        place-items: center;
+    }
+</style>
